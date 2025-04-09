92c43d985780d32287acd85fb576f61a67d4f390
Filip <fhagno@yahoo.com>
2025-04-09 11:18:26 +0200

    Init

---
+++ src/test/java/pl/filiphagno/spring6icecoldservice/Spring6IcecoldServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.filiphagno.spring6icecoldservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6IcecoldServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
